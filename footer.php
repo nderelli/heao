e8407fdc7173beb0dcd104cf55b8c54bea1927d2
nderelli <peter@anderelli.com>
2013-03-07 01:20:19 -0700

    Linked to jquery library

--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
-		</div> <!-- End .content -->
+		</div> <!-- End .content-wrapper -->
 		
 		<footer>
 		</footer>
 
-	</div> <!-- Close .wrapper !-->
+	</div> <!-- Close .container !-->
 
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <?php wp_enqueue_script( 'js-combo', get_bloginfo( 'template_directory' ) . '/js/script.js', array('jquery')); ?>
 
 <?php wp_footer(); ?><!-- required don't remove -->
